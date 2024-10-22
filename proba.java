2036386dde446607d6921c5dbeb02c3a97028274
adminikt <adminikt@fpsanturtzilh.eus>
2024-10-22 09:37:07 +0200

    hirugarrena

--- proba.java
+++ proba.java
@@ -3,6 +3,6 @@ package ariketak;
 public class PROG_E1_Java_Ariketa02 {
 
 	public static void main(String[] args) {
-		System.out.println("Kaixo git!!!!!!!!!");
+		System.out.println("Kaixo git!!!!!!!!!!!!!!!!");
 	}
 }
